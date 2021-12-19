02e8e146ba20e3ca60624751ad58194096b18e09
Abhishek Mukherjee <am2455@njit.edu>
2021-12-18 21:43:44 -0500

    update dissipation

--- tsunami/dissipation_rate.py
+++ tsunami/dissipation_rate.py
@@ -34,11 +34,11 @@ def epsilon(xpt, ypt, zpt, lev_3d, mask_3d, x_3d, y_3d, z_3d, v3d):
     S[4, :, :] = 0.5 * (gradU[5, :, :, :] + gradU[7, :, :, :])
     S[5, :, :] = 1 * gradU[8, :, :, :]
 
-    epsilon = 2 * 1e-6 * (S[0, :, :] * S[0, :, :] + 2 * S[1, :, :] + 2 * S[2, :, :] + S[3, :, :] +
-                          2 * S[4, :, :] + S[5, :, :])
+    epsilon = (S[0, :, :] * S[0, :, :] + 2 * S[1, :, :] * S[1, :, :] + 2 * S[2, :, :] * S[2, :, :] +
+               S[3, :, :] * S[3, :, :] + 2 * S[4, :, :] * S[4, :, :] + S[5, :, :] * S[5, :, :])
     # # dissipation rate based on instantaneous velocity
 
-    epsi_2d_z, con_z, depth_2d = depth_average(lev_3d, z_3d, epsilon, xpt, ypt, zpt, c_3d, 1)
-    epsi_1d, con_y = field_avg(1 * epsi_2d_z, 1, xpt, ypt, zpt, 'y')
+    #epsi_2d_z, con_z, depth_2d = depth_average(lev_3d, z_3d, epsilon, xpt, ypt, zpt, c_3d, 1)
+    #epsi_1d, con_y = field_avg(1 * epsi_2d_z, 1, xpt, ypt, zpt, 'y')
 
-    return epsi_1d[:, 0, 0]
+    return epsilon
